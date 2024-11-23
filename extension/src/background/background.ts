072eb4e7b794f2db5686c341139592c8acb325d6
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2024-11-23 23:32:37 +0530

    extension idea scapped

--- extension/src/background/background.ts
+++
@@ -1,9 +0,0 @@
-import extractArticleBody from '../contentScript/contentScript';
-
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: extractArticleBody,
-    args: ['default', '']  // Provide default method and selectors
-  });
-});
